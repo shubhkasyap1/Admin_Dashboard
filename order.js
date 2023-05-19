fe689d0853c1970401954f67d0f57adc24c535ea
shubhkasyap1 <shubham002001@gmail.com>
2023-05-20 00:31:48 +0530

    first commit

---
+++ order.js
@@ -0,0 +1,36 @@
+const Orders = [
+    {
+        productName:'Foldable Mini Drone',
+        ProductNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+
+    {
+        productName:'Foldable Mini Drone',
+        ProductNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+
+    {
+        productName:'Foldable Mini Drone',
+        ProductNumber: '85631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+
+    {
+        productName:'Foldable Mini Drone',
+        ProductNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+
+    {
+        productName:'Foldable Mini Drone',
+        ProductNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Deliverd',
+    },
+]
